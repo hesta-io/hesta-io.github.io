1086c6027077ba6562d641b6fd8657057c209c39
Aram Rafeq <aramrafeq2@gmail.com>
2022-12-18 22:41:25 +0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <link rel="stylesheet" href="/style.css">
+</svelte:head>
+
+<h1>hello world from layout</h1>
+<pre>
+    <slot></slot>
+</pre>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<b>index page </b>
