e2f203188708fb450f1f9767f2ba1275852ce477
Aram Rafeq <aramrafeq2@gmail.com>
2023-05-22 21:43:33 +0300

    pre final build

--- out/_next/static/vbSd8TDQBPuOa0spXkCnV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/390-2835910fb0b844a6.js","static/css/4df78f2cd73d6b26.css","static/chunks/pages/index-5f566a958ecd9dd0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
